e45ee0b6a2f29f98acaa1bc667ea561813a2b755
Samuel Ifere <applicationdev@nesistec.co.uk>
2019-10-06 15:55:24 +0100

    Adds sample code for null checks and handling

---
+++ src/nullableExample.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var nullValue: String? = null
+    println("Value is: $nullValue")
+
+    val leng: Int? = nullValue?.length
+
+    val message = if (leng != null) "Length: $leng" else "Length is null"
+    println(message)
+
+    val leng2: Int = leng ?: -1
+    println("The value of length2 is $leng2")
+
+    try {
+        val leng3 = leng!! //used to assert the value is null
+        println("The value of length 3 is $leng3")
+    } catch (knpe: KotlinNullPointerException) {
+        println("Length 3 is null: ${knpe.message}")
+    }
+}
