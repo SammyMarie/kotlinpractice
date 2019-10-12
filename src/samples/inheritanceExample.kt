28da2fbf3267008ac07f23011e3187570f00e222
Samuel Ifere <applicationdev@nesistec.co.uk>
2019-10-12 16:35:07 +0100

    Adds logic to override methods from a super class.

--- src/samples/inheritanceExample.kt
+++ src/samples/inheritanceExample.kt
@@ -9,14 +9,25 @@ fun main() {
 
     val sub = SubClass(123)
     println(sub)
+
+    println("Superclass operation: ${sup.multiply(23)}")
+    println("Subclass operation: ${sub.multiply(5)}")
 }
 
 open class SuperClass(anInt: Int) {
     protected val _anInt = anInt
 
     override fun toString(): String {
-        return "${this::class.simpleName} {anInt: $_anInt}"
+        return "${this::class.simpleName}: {anInt: $_anInt}"
+    }
+
+    open fun multiply(factor: Int): Int {
+        return _anInt.times(factor)
     }
 }
 
-class SubClass(anInt: Int) : SuperClass(anInt)
+class SubClass(anInt: Int) : SuperClass(anInt) {
+    override fun multiply(factor: Int): Int {
+        return super.multiply(factor).times(factor)
+    }
+}
